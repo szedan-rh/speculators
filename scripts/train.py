6362f87da8de01ff62653ac88148b8a2a65b8cbe
shanjiaz <zsjwpianpian@gmail.com>
2026-07-05 09:53:32 +0000

    format
    Signed-off-by: shanjiaz <zsjwpianpian@gmail.com>

--- scripts/train.py
+++ scripts/train.py
@@ -166,7 +166,12 @@ def create_transformer_layer_config(  # noqa: C901
                 }
             config.rope_parameters = rope_params
 
-            _MROPE_KEYS = ("mrope_section", "mrope_interleaved", "type", "partial_rotary_factor")  # noqa: N806
+            _MROPE_KEYS = (  # noqa: N806
+                "mrope_section",
+                "mrope_interleaved",
+                "type",
+                "partial_rotary_factor",
+            )
             for key in _MROPE_KEYS:
                 config.rope_parameters.pop(key, None)
     else:
